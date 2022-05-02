e9cd53dc5d745247e1a595059de0ab469fe2247a
HarishKamaras <harishkamarasu@gmail.com>
2022-05-02 16:07:02 +0530

    Added : EqualitysOfLines

--- src/com/brigdelabz/comparison/LineComparison.java
+++ src/com/brigdelabz/comparison/LineComparison.java
@@ -24,6 +24,13 @@ public class LineComparison {
         len2 = Math.sqrt((x4 - x3) * (x4 - x3) + (y4 - y3) * (y4 - y3));
         System.out.println("Length between first line" + "(" + x1 + "," + y1 + ")," + "(" + x2 + "," + y2 + ")===>" + len1);
         System.out.println("Length between second line" + "(" + x3 + "," + y3 + ")," + "(" + x4 + "," + y4 + ")===>" + len2);
-
-    }
+        //UC2 equality of two lines
+        Double d1 = new Double(len1);
+        Double d2 = new Double(len2);
+        if (d1.equals(d2)) {
+            System.out.println("len1 equals to len2");
+        } else {
+            System.out.println("len1 not equals to len2");
+        }
+        }
     }
